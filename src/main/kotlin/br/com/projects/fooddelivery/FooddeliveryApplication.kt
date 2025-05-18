af72421cfa021f949ef8f0400495270ba3527e55
Gabriel <paschoalfilho0303@gmail.com>
2025-05-18 18:20:37 -0300

    feature: setup kotlin project

---
+++ src/main/kotlin/br/com/projects/fooddelivery/FooddeliveryApplication.kt
@@ -0,0 +1,11 @@
+package br.com.projects.fooddelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FooddeliveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<FooddeliveryApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/projects/fooddelivery/FooddeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.projects.fooddelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FooddeliveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
